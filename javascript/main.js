a10d389959840da8f5216cc134829a46cdfc76d8
Hossein <hpourjafarabadi.dev@gmail.com>
2025-06-27 22:18:10 +0330

    General completion and setting up

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,5 @@
-let parentBox = document.getElementById('parentBox');
-
+const parentBox = document.getElementById('parentBox');
+const scores = document.querySelectorAll('.score h2 span');
 let boxes = [];
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,6 +21,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     ballRandom()
+
+    if (localStorage.getItem('score')) {
+        scores[0].textContent = localStorage.getItem('score').split(',');
+    }
+    else {
+        document.querySelector('.score h1').textContent = 1;
+    }
 })
 
 let intervalId;
@@ -166,6 +173,8 @@ function move(m) {
         if (boxes[n].children[0].classList.contains('head') == true && boxes[n].children[0].classList.contains('ball') == true) {
             ballRandom();
             backType();
+            saveData();
+            scores[1].textContent = marhale.length;
         }
 
         createTail(n);
@@ -212,3 +221,342 @@ function lose(n) {
         window.location.reload();
     }, 2000);
 }
+
+//////////////////////////////////////localStorage
+
+function saveData() {
+    if (localStorage.getItem('score') === null) {
+        localStorage.setItem('score', marhale.length);
+    }
+    else {
+        if (localStorage.getItem('score') < marhale.length) {
+            localStorage.setItem('score', marhale.length)
+        }
+    }
+}
+
+//////////////////////////////////////settings
+
+const settings = document.getElementsByClassName('settings')[0];
+const parentBoxesSettings = document.getElementsByClassName('parentBoxesSettings')[0];
+
+settings.addEventListener('click', () => {
+    parentBoxesSettings.classList.toggle('settingsHeightClick');
+    settings.children[0].classList.toggle('settingsRotateClick');
+    settings.children[2].classList.toggle('settingsRotateClick');
+})
+
+
+const body = document.getElementsByTagName('body')[0];
+class PositionColorFullScreen {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        body.style.backgroundColor = this.colorLocal;
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(1) input[type=color]').value;
+        body.style.backgroundColor = color;
+        settingsLoc('FullScreen', color);
+    }
+}
+let FullScreenShow = new PositionColorFullScreen();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(1) input[type=button]').addEventListener('click', FullScreenShow.show.bind(FullScreenShow));
+
+
+class PositionColorGameFrame {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        parentBox.style.backgroundColor = this.colorLocal;
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(2) input[type=color]').value;
+        parentBox.style.backgroundColor = color;
+        settingsLoc('GameFrame', color);
+    }
+}
+let GameFrameShow = new PositionColorGameFrame();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(2) input[type=button]').addEventListener('click', GameFrameShow.show.bind(GameFrameShow));
+
+
+const sidebar = document.getElementsByClassName('sidebar')[0];
+class PositionColorSidebarFrame {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        sidebar.style.backgroundColor = this.colorLocal;
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(3) input[type=color]').value;
+        sidebar.style.backgroundColor = color;
+        settingsLoc('SidebarFrame', color);
+    }
+}
+let SidebarFrameShow = new PositionColorSidebarFrame();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(3) input[type=button]').addEventListener('click', SidebarFrameShow.show.bind(SidebarFrameShow));
+
+
+class PositionColorTextColor {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        document.documentElement.style.setProperty('--textColor', this.colorLocal);
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(4) input[type=color]').value;
+        document.documentElement.style.setProperty('--textColor', color);
+        settingsLoc('TextColor', color);
+    }
+}
+let TextColorShow = new PositionColorTextColor();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(4) input[type=button]').addEventListener('click', TextColorShow.show.bind(TextColorShow));
+
+
+const btns = Array.from(document.getElementsByClassName('btn'));
+class PositionColorButtonColor {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        btns.forEach(element => {
+            element.style.backgroundColor = this.colorLocal;
+        })
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(5) input[type=color]').value;
+        btns.forEach(element => {
+            element.style.backgroundColor = color;
+        })
+        settingsLoc('ButtonColor', color);
+    }
+}
+let ButtonColorShow = new PositionColorButtonColor();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(5) input[type=button]').addEventListener('click', ButtonColorShow.show.bind(ButtonColorShow));
+
+
+class PositionColorButtonIndicatorColor {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        btns.forEach(element => {
+            element.style.color = this.colorLocal;
+        })
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(6) input[type=color]').value;
+        btns.forEach(element => {
+            element.style.color = color;
+        })
+        settingsLoc('ButtonIndicatorColor', color);
+    }
+}
+let ButtonIndicatorColorShow = new PositionColorButtonIndicatorColor();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(6) input[type=button]').addEventListener('click', ButtonIndicatorColorShow.show.bind(ButtonIndicatorColorShow));
+
+
+class PositionColorSnakeBody {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        document.documentElement.style.setProperty('--colorTail', this.colorLocal)
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(7) input[type=color]').value;
+        document.documentElement.style.setProperty('--colorTail', color)
+        settingsLoc('SnakeBody', color);
+    }
+}
+let SnakeBodyShow = new PositionColorSnakeBody();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(7) input[type=button]').addEventListener('click', SnakeBodyShow.show.bind(SnakeBodyShow));
+
+
+class PositionColorSnakeHead {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        document.documentElement.style.setProperty('--colorHead', this.colorLocal)
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(8) input[type=color]').value;
+        document.documentElement.style.setProperty('--colorHead', color)
+        settingsLoc('SnakeHead', color);
+    }
+}
+let SnakeHeadShow = new PositionColorSnakeHead();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(8) input[type=button]').addEventListener('click', SnakeHeadShow.show.bind(SnakeHeadShow));
+
+
+class PositionColorFoodColor {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        document.documentElement.style.setProperty('--colorBall', this.colorLocal)
+    }
+    show() {
+        let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(9) input[type=color]').value;
+        document.documentElement.style.setProperty('--colorBall', color)
+        settingsLoc('FoodColor', color);
+    }
+}
+let FoodColorShow = new PositionColorFoodColor();
+parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(9) input[type=button]').addEventListener('click', FoodColorShow.show.bind(FoodColorShow));
+
+
+const checkboxBorder = parentBoxesSettings.querySelector('.boxChooseBoard input[type=checkbox]');
+class PositionColorBorderBoxes {
+    constructor(colorLocal) {
+        this.colorLocal = colorLocal;
+    }
+    sendy() {
+        checkboxBorder.checked = this.colorLocal;
+        if (this.colorLocal) {
+            boxes.forEach(element => {
+                element.classList.add('boxBorderClick');
+            })
+        }
+        else {
+            boxes.forEach(element => {
+                element.classList.remove('boxBorderClick');
+            })
+        }
+    }
+    show() {
+        if (checkboxBorder.checked) {
+            boxes.forEach(element => {
+                element.classList.add('boxBorderClick');
+            })
+        }
+        else {
+            boxes.forEach(element => {
+                element.classList.remove('boxBorderClick');
+            })
+        }
+        settingsLoc('BorderBoxes', checkboxBorder.checked);
+    }
+}
+let BorderBoxesShow = new PositionColorBorderBoxes();
+checkboxBorder.addEventListener('change', BorderBoxesShow.show.bind(BorderBoxesShow));
+
+
+// class PositionColorbloodColor {
+//     constructor(colorLocal) {
+//         this.colorLocal = colorLocal;
+//     }
+//     sendy() {
+//     }
+//     show() {
+//         let color = parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(10) input[type=color]').value;
+//         settingsLoc('bloodColor', color);
+//     }
+// }
+// let bloodColorShow = new PositionColorbloodColor();
+// parentBoxesSettings.querySelector('.boxChooseColor:nth-of-type(10) input[type=button]').addEventListener('click', bloodColorShow.show.bind(bloodColorShow));
+
+
+//////////////////////////////////////localStorage settings
+
+function settingsLoc(key, value) {
+
+    let colors;
+
+    if (localStorage.getItem('personalization') === null) {
+        colors = new Map();
+    }
+    else {
+        colors = new Map(JSON.parse(localStorage.getItem('personalization')));
+    }
+
+    colors.set(key, value);
+
+    localStorage.setItem('personalization', JSON.stringify([...colors]));
+    let getLocal = new Map(JSON.parse(localStorage.getItem('personalization')));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let getLocal = new Map(JSON.parse(localStorage.getItem('personalization')));
+
+    if (getLocal.get('FullScreen')) {
+        let FullScreenSendy = new PositionColorFullScreen(getLocal.get('FullScreen'));
+        FullScreenSendy.sendy();
+    }
+    if (getLocal.get('GameFrame')) {
+        let GameFrameSendy = new PositionColorGameFrame(getLocal.get('GameFrame'));
+        GameFrameSendy.sendy();
+    }
+    if (getLocal.get('SidebarFrame')) {
+        let SidebarFrameSendy = new PositionColorSidebarFrame(getLocal.get('SidebarFrame'));
+        SidebarFrameSendy.sendy();
+    }
+    if (getLocal.get('TextColor')) {
+        let TextColorSendy = new PositionColorTextColor(getLocal.get('TextColor'));
+        TextColorSendy.sendy();
+    }
+    if (getLocal.get('ButtonColor')) {
+        let ButtonColorSendy = new PositionColorButtonColor(getLocal.get('ButtonColor'));
+        ButtonColorSendy.sendy();
+    }
+    if (getLocal.get('ButtonIndicatorColor')) {
+        let ButtonIndicatorColorSendy = new PositionColorButtonIndicatorColor(getLocal.get('ButtonIndicatorColor'));
+        ButtonIndicatorColorSendy.sendy();
+    }
+    if (getLocal.get('SnakeBody')) {
+        let SnakeBodySendy = new PositionColorSnakeBody(getLocal.get('SnakeBody'));
+        SnakeBodySendy.sendy();
+    }
+    if (getLocal.get('SnakeHead')) {
+        let SnakeHeadSendy = new PositionColorSnakeHead(getLocal.get('SnakeHead'));
+        SnakeHeadSendy.sendy();
+    }
+    if (getLocal.get('FoodColor')) {
+        let FoodColorSendy = new PositionColorFoodColor(getLocal.get('FoodColor'));
+        FoodColorSendy.sendy();
+    }
+    // if (getLocal.get('bloodColor')) {
+    //     let bloodColorSendy = new PositionColorbloodColor(getLocal.get('bloodColor'));
+    //     bloodColorSendy.sendy();
+    // }
+    if (getLocal.get('BorderBoxes')) {
+        let BorderBoxesSendy = new PositionColorBorderBoxes(getLocal.get('BorderBoxes'));
+        BorderBoxesSendy.sendy();
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let colors = {
+//     FullScreen: FullScreen(),
+//     GameFrame: GameFrame(),
+//     SidebarFrame: SidebarFrame(),
+//     TextColor: TextColor(),
+//     ButtonColor: ButtonColor(),
+//     ButtonIndicatorColor: ButtonIndicatorColor(),
+//     SnakeBody: SnakeBody(),
+//     SnakeHead: SnakeHead(),
+//     FoodColor: FoodColor(),
+//     bloodColor: bloodColor(),
+//     BorderBoxes: BorderBoxes()
+// }
